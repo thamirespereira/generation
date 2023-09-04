b7d2bce72e60f34b26d14c3a5e65abb8263a6111
thamirespereira <thamiresfrpereira@hotmail.com>
2023-09-04 12:48:53 -0300

    exercício vetor

---
+++ arrays/exercicio1.java
@@ -0,0 +1,42 @@
+package arrays;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+        
+        int vetor [] = new int[10];
+        int soma = 0;
+        int par = 0;
+        int impar =0;
+        Scanner leia = new Scanner(System.in);
+
+        for(int indice = 0; indice < 10; indice++){
+            System.out.println("Digite um número:");
+            vetor[indice] = leia.nextInt();
+            if (indice % 2 != 0) {
+                    impar++;
+                }
+              
+                }
+
+            for (int i : vetor) {
+                if (vetor[i]%2==0) {
+                 par++;
+            }}
+
+            for (Integer i : vetor) {
+                soma += i.intValue();
+            } 
+            System.out.println("Elementos nos indices ímpares:" + impar);
+
+            System.out.println("Elementos do vetor que são números pares: " + par);
+
+            System.out.println("Soma de todos os elementos do vetor: " + soma);
+        
+            System.out.println("Média de todos os elementos do vetor: " + (soma/10));
+        }
+        
+        
+    }
+
