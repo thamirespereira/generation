4fe45e02889c0f8d3eef417618c0d54f0826db8b
thamirespereira <thamiresfrpereira@hotmail.com>
2023-09-08 11:59:57 -0300

    exercício com collection set

--- collections/numeros.java
+++ collections/numeros.java
@@ -4,6 +4,11 @@ import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
+/*Escreva um programa Java para criar uma Collection Set de Objetos da Classe Wrapper Integer. 
+O programa deverá solicitar ao usuário, que ele digite via teclado 10 valores inteiros não repetidos e 
+adicione-os individualmente na Collection Set. Em seguida, faça o que se pede:
+Mostre na tela todos os elementos da Collection Set, utilizando a Classe Iterator. */
+
 public class numeros {
     public static void main(String[] args) {
         
