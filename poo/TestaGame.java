516128841aa81ca5f11f54a6b448fe9b4d9f1be1
thamirespereira <thamiresfrpereira@hotmail.com>
2023-09-18 14:09:29 -0300

    exercício stream

--- poo/TestaGame.java
+++ poo/TestaGame.java
@@ -1,10 +1,19 @@
 package poo;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class TestaGame {
+
     public static void main(String[] args) {
         
         Produto produto1 = new Produto("The sims", "simulacao", 2017, 132);
         Produto produto2 = new Produto("World of Warcraft", "MMORPG", 2014, 230);
+
+         List <String> jogo = Arrays.asList("sudoku", "the sims", "wow", "lol");
+        Stream <String> novoJogo = new jogo.stream();
 
         produto1.Visualizar();
         System.out.println("-----------------");
