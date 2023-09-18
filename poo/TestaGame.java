a9e6f2bc28c5f01d9e3f9f64c70e8460b78503a8
thamirespereira <thamiresfrpereira@hotmail.com>
2023-09-18 14:12:28 -0300

    exercício stream

--- poo/TestaGame.java
+++ poo/TestaGame.java
@@ -3,6 +3,7 @@ package poo;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TestaGame {
@@ -12,8 +13,14 @@ public class TestaGame {
         Produto produto1 = new Produto("The sims", "simulacao", 2017, 132);
         Produto produto2 = new Produto("World of Warcraft", "MMORPG", 2014, 230);
 
-         List <String> jogo = Arrays.asList("sudoku", "the sims", "wow", "lol");
-        Stream <String> novoJogo = new jogo.stream();
+        List <String> jogo = Arrays.asList("sudoku", "the sims", "wow", "lol", "subway suffers");
+	
+	        List <String> jogoS = jogo.stream()
+			        .filter(x-> x.toString().startsWith("s"))
+			        .sorted()
+			        .collect(Collectors.toList());
+
+			System.out.println("\nJogos cujo nome começam com a letra R: " + jogoS);
 
         produto1.Visualizar();
         System.out.println("-----------------");
